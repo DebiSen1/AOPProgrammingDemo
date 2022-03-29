fb2da9f5e20c78bee21bd589bf55a1dc4a096678
DebiSen1 <sdebi266@gmail.com>
2022-03-29 09:55:32 +0530

    Logging aspect has been added to see the logging info

--- src/main/java/com/upgrad/course/demo/aspect/LoggingAspect.java
+++ src/main/java/com/upgrad/course/demo/aspect/LoggingAspect.java
@@ -5,13 +5,30 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+/**
+ * To make it a class where aspect oriented programming features will be used mark it with @Aspect annotation
+ * and to create its bean mark it with @Component*/
 
 @Aspect
 @Component
 public class LoggingAspect {
 
+/**
+ * Advice - It is the action taken by an aspect at a particular join-point. Joinpoint is a point of execution of the
+ * program, such as the execution of a method or the handling of an exception. In Spring AOP, a joinpoint always
+ * represents a method execution.
+ *
+ * @Around is an advice type, which ensures that an advice can run before and after the method execution.
+ * Following is the syntax of @Around advice.
+ * */
+
     @Around("execution(* com.upgrad.course.demo..*(..))")
     public Object applyLogging(ProceedingJoinPoint joinPoint) throws Throwable{
+
+        /**
+         * while creating the business logic of the method the additional services are needed to be injected at
+         * different places or points, we call such points as join points.  At a join point a new services will be
+         * added into the normal flow of a business method.*/
 
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
 
